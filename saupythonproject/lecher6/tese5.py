b780b9088a71231fb190bee7115abade63ab5594
6652410016 kittipon <143469099+6652410016Kittipol@users.noreply.github.com>
2023-09-12 12:59:59 +0700

    Add files via upload

---
+++ saupythonproject/lecher6/tese5.py
@@ -0,0 +1,16 @@
+# คำสั่ง for
+
+for x in range (5):
+        print (f"DTI-{x}")
+print("__________________________")
+for y in range (2,5) :
+        print (f"SAU-{y}")
+print ("__________________________")
+
+for z in range (2,5,3) :
+    print (f"OMG-{z}")
+print ("________________________________")
+
+for a in range (3,30,5) :
+    print (f"Wow-{a}")
+    
---
+++ saupythonproject/lecher6/tese8.py
@@ -0,0 +1,18 @@
+
+
+
+#วิเตราะห์
+#แมองหา feature การทำางานว่ามีะไรบำาง เพื่อจะไปสร้างเป็น function
+#1. รับค่า ฐาน สูง
+#2. ตำนวณพื้นที และแสดงผลออกมา
+
+def inputBaseHigh()  :
+    base = float ( input('ป้อนฐาน :'))
+    high a float( input('ป้อนสูง  :'))
+    return base, high
+
+
+print('_____________________________________')
+print('--" Calculate Trlangle Area "--')
+print('_____________________________________')
+base, high = inputBaseHigh()
